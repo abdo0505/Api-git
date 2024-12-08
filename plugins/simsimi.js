eb3aa3638d2850ae90f5b936724819f67544e45c
aayoubayoub2781 <148594510+aayoubayoub2781@users.noreply.github.com>
2024-12-08 15:54:36 +0100

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -31,7 +31,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 handler.help = ['bot'];
 handler.tags = ['fun'];
-handler.command = ['سمسم'];
+handler.command = ['zizo'];
 handler.owner = false;
 
 
