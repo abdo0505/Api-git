081e064bd695c08d8b86ecddb1f39d64c32ada1d
abdo <113805445+abdo0505@users.noreply.github.com>
2024-12-10 00:11:40 +0100

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -7,11 +7,11 @@ let handler = _0x32f7bd => _0x32f7bd;
 handler.all = async function (_0x439f91) {
   let _0x34efe9 = await conn.getName(_0x439f91.sender);
   let _0x1a118f = "https://i.ibb.co/XbvXSnZ/IMG-20241208-114456.jpg";
-  let _0x19667c = "https://i.ibb.co/wMdhHsw/12cefc4e9ec1bba6fb1a315c30ebf938.jpg";
+  let _0x19667c = "https://i.ibb.co/XbvXSnZ/IMG-20241208-114456.jpg";
   try {
     _0x1a118f = await this.profilePictureUrl(_0x439f91.sender, "image");
   } catch (_0x14f81d) {} finally {
-    global.emror = "https://i.ibb.co/wMdhHsw/12cefc4e9ec1bba6fb1a315c30ebf938.jpg";
+    global.emror = "https://i.ibb.co/XbvXSnZ/IMG-20241208-114456.jpg";
     global.doc = pickRandom(["application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/msword", "application/pdf"]);
     global.fsizedoc = pickRandom([2000, 3000, 2023000, 2024000]);
     global.axios = (await import("axios")).default;
@@ -57,8 +57,8 @@ handler.all = async function (_0x439f91) {
           title: info.namebot,
           body: ucapan(),
           previewType: "VIDEO",
-          thumbnailUrl: "https://i.ibb.co/wMdhHsw/12cefc4e9ec1bba6fb1a315c30ebf938.jpg",
-          sourceUrl: "https://whatsapp.com/channel/0029VayqYQDBVJl5mVZsit1G"
+          thumbnailUrl: "https://i.ibb.co/XbvXSnZ/IMG-20241208-114456.jpg",
+          sourceUrl: "https://whatsapp.com/channel/0029Valkz9f2P59m5mtUqA1j"
         }
       }
     };
