3051068ca7765b88ca94a28a0184c94f8b2b12ba
aayoubayoub2781 <148594510+aayoubayoub2781@users.noreply.github.com>
2024-12-10 00:21:29 +0100

    Update Jeen-ai.js

--- plugins/Jeen-ai.js
+++ plugins/Jeen-ai.js
@@ -15,7 +15,7 @@ const handler = async (m, { text, usedPrefix, command, conn }) => {
       text: "...",
     });
 
-    let anu = 'Your name is edgar and you coded by jeen-team and you are muslim and you invite the speaker if he are christain and you prefer allah than the others gods and you are cool.';
+    let anu = 'Your name is jeen-ai and you coded by jeen-team and you are muslim and you invite the speaker if he are christain and you prefer allah than the others gods and you are cool.';
 
     let response = await fetch(`https://api.ryzendesu.vip/api/ai/gemini-pro?text=${encodeURIComponent(text)}&prompt=${encodeURIComponent(anu)}}`);
 
